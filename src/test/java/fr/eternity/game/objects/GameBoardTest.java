4e2f953d5c0811c05470dbbf85562a7e8affb67b
Gary HOST <ghost@iPhone-de-Kevin>
2015-05-15 18:24:11 +0200

    Ajout des modifications des tests pour la classe GameBoardTest

--- src/test/java/fr/eternity/game/objects/GameBoardTest.java
+++ src/test/java/fr/eternity/game/objects/GameBoardTest.java
@@ -1,7 +1,5 @@
 package fr.eternity.game.objects;
 
-import java.util.ArrayList;
-
 import junit.framework.TestCase;
 
 public class GameBoardTest extends TestCase {
@@ -22,12 +20,20 @@ public class GameBoardTest extends TestCase {
 		assertEquals(p3, gameboard.getPiece(3,2));
 	}
 
-	public void testDrawGameBoard() {
-		fail("Not yet implemented");
-	}
-
 	public void testGetPiece() {
-		fail("Not yet implemented");
+		GameBoard gameBoard = new GameBoard(8);
+		
+		Piece p1 = new Piece();
+		Piece p2 = new Piece();
+		Piece p3 = new Piece();
+		
+		gameBoard.setPiece(4, 2, p1);
+		gameBoard.setPiece(3, 0, p2);
+		gameBoard.setPiece(3, 2, p3);
+		
+		assertEquals(p1, gameBoard.getPiece(4,2));
+		assertEquals(p2, gameBoard.getPiece(3,0));
+		assertEquals(p3, gameBoard.getPiece(3,2));
 	}
 
 }
